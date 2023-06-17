08b1eff7e15958af105b16055f7783c13e4d3b94
Ye Myat Htay <ymyat213@gmail.com>
2023-06-17 20:02:01 +0630

    second commit

--- src/App.js
+++ src/App.js
@@ -12,8 +12,14 @@ class Item extends React.Component {
 class App extends React.Component {
   state = {items: [{id:1,name:'apple',price:0.8},{id:2,name:'banana',price:0.5}]}
 
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+
   add = () => {
     let id = this.state.items.length+1;
+    let name = this.nameRef.current.value;
+    let price = this.priceRef.current.value;
+
     this.setState({
       items: [
         ...this.state.items,
@@ -31,6 +37,8 @@ class App extends React.Component {
                 <Item key={i.id} name={i.name} price={i.price} />
               )})}
           </ul>
+          <input type="text" ref={this.nameRef}/><br/>
+          <input type="text" ref={this.priceRef}/><br/>
           <button onClick={this.add}>Add</button>
       </div>
     )
